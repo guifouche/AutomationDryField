98a7983df17a7ee38206a170ccc60f7f42410e65
Vincent Lene <vincent.lene.dl@gmail.com>
2018-02-15 11:50:07 +0100

    added karma sauce launcher to plugin array

--- front/src/config/webpack.test.js
+++ front/src/config/webpack.test.js
@@ -185,6 +185,10 @@ module.exports = function (options) {
      * See: http://webpack.github.io/docs/configuration.html#plugins
      */
     plugins: [
+      /**
+       * Plugin: karma-sauce-launcher
+       */
+      'karma-sauce-launcher',
 
       /**
        * Plugin: DefinePlugin
